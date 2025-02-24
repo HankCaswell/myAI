d314c83ba0f3a2764aa869d5abea3e3dd929724d
Hank Caswell <david.h.caswell@proton.me>
2025-02-24 16:26:05 -0500

    edited ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `[FOOTER MESSAGE]`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const CHAT_HEADER: string = `Veteran's Guide to Business School Application`;
+export const MESSAGE_PLACEHOLDER: string = `Send a message`;
+export const FOOTER_MESSAGE: string = `Created for MBA742 at UNC Kenan-Flagler under a creative commons license`;
+export const CLEAR_BUTTON_TEXT: string = `Clear Conversation`;
+export const PAGE_TITLE: string = `Vet Business AI`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
