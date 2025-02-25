97d5a4ffa3cda619e68c9c58485c57e42151a6e6
Hank Caswell <david.h.caswell@proton.me>
2025-02-25 10:40:39 -0500

    changed title

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,8 +1,8 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Veteran's Guide to Business School Application`;
+export const CHAT_HEADER: string = `Veteran Transition Assistant`;
 export const MESSAGE_PLACEHOLDER: string = `Send a message`;
-export const FOOTER_MESSAGE: string = `Created for MBA742 at UNC Kenan-Flagler under a creative commons license`;
+export const FOOTER_MESSAGE: string = `Created for MBA742 at UNC Kenan-Flagler`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Conversation`;
 export const PAGE_TITLE: string = `Vet Business AI`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
