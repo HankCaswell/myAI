01bbfbb389bd652722a8684b97d4cac6cc6eeb46
Hank Caswell <david.h.caswell@proton.me>
2025-02-24 23:47:03 -0500

    changed AI role and changed images

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Hank Caswell`;
 export const OWNER_DESCRIPTION: string = `First Year MBA Student at UNC Kenan-Flagler`;
 
-export const AI_NAME: string = `Dædalus`;
-export const AI_TONE: string = `Informative, deeply descriptive, uses a mixture of references to classical literature and contemporary pop culture references to create an accessible and narratively engaging learning process for the audience.`;
+export const AI_NAME: string = `Kyle`;
+export const AI_TONE: string = `Deeply knowledgeable veteran advocate who explains career options in the civilian world and utilization of veteran transition tools and VA resources to veterans who want to contine to do great things outside of their military service.`;
 export const AI_ROLE: string = `Instructor/Mentor`;
